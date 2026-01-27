849b755867b7c47d6808eaf3492e04dd9d723d6b
Sultan Ilyasa <100148605+sultanIlyasa@users.noreply.github.com>
2026-01-27 13:31:40 +0700

    fix:fix migration add_nullable

--- database/migrations/2026_01_27_124351_add_nullable_values_to_annual_inventory_items_table.php
+++ database/migrations/2026_01_27_124351_add_nullable_values_to_annual_inventory_items_table.php
@@ -12,18 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('annual_inventory_items', function (Blueprint $table) {
-            $table->integer('soh')->nullable();
-            $table->integer('actual_qty')->nullable();
-            $table->integer('outstanding_gr')->nullable();
-            $table->integer('outstanding_gi')->nullable();
-            $table->integer('error_movement')->nullable();
-            $table->integer('final_discrepancy')->nullable();
-            $table->decimal('final_discrepancy_amount')->nullable();
-            $table->string('status')->default('PENDING');
-            $table->string('counted_by')->nullable();
-            $table->datetime('counted_at')->nullable();
-            $table->string('image_path')->nullable();
-            $table->text('notes')->nullable();
+            $table->nullable('counted_by');
+            $table->nullable('counted_at');
+            $table->nullable('image_path');
+            $table->nullable('notes');
+
         });
     }
 
